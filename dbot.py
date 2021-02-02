506bc09cec05b32e14b6d6b243a948024a86cad3
Branden Gist <breezygeazygist2121@gmail.com>
2021-02-02 16:26:01 -0500

    Add files via upload
    updated with new responses. next the bot is able to generate different gifs

--- dbot.py
+++ dbot.py
@@ -4,12 +4,50 @@ import os
 import requests
 import json
 import random
-import replit from db
+import replit 
 
 # Client (our bot)
 client = discord.Client()
 
-sad_words = ["sad", "depressed", "unhappy", "angry", "sorrow", "saaad", "dead", "death", "mourning", "miserable", "depressing", "suffer"]
+hello_words = ["Hey", "Sup", "Heyy", "heyyy", "hey", ":grin:", ":wave: heya", "how are ya", "heya", "what's up", "hi"]
+# Part Of hello words in response to you
+converstaion_words = ["how are you", ""]
+
+love_words = ["love you", "luv u", ">3"]
+
+sad_words = ["sad", "depressed", "unhappy", "angry", "sorrow", "saaad", "dead", "death", "mourning", "miserable", "depressing", "suffer", "down"]
+
+starter_greetings = [
+   "Hey", 
+   "Sup", 
+   "Heyy", 
+   "heyyy", 
+   "hey", 
+   ":grin:", 
+   ":wave: heya",
+   "how are ya", 
+   "heya", 
+   "what's up",
+   "hi"
+]
+
+starter_love = [
+    ":Kissing_heart: You're always so nice to me",
+    ":smiling_face_with_3_hearts: how's your day been going",
+    ":smiling_face_with_3_hearts: love you too",
+    "I'm grateful for you!",
+    "https://tenor.com/view/hug-virtual-hug-hug-sent-gif-5026057 lucky you",
+    ":heart_eyes: we should stream a movie together!"
+]
+
+starter_converstaion = [
+    "I've been doing good, busy as always!",
+    "Same as always what's new with you?",
+    "Really?!"
+    "That's pretty cool!",
+    "Watching any new shows?"
+    
+]
 
 starter_encouragements = [
     "Cheer Up, I'm here for!",
@@ -22,18 +60,26 @@ starter_encouragements = [
     ":sob:",
     ":cry:",
     ":cry: Don't say that!",
-    ":sob: I'm here!"
+    ":sob: I'm here!",
+    "Don't say that!",
+    "You can talk to me!"
 ]
 
-
+# Insprational Quotes
 def get_quote():
     response = requests.get("https://zenquotes.io/api/random")
     json_data = json.loads(response.text)
     quote = json_data[0]['q'] + " -" + json_data[0]['a']
     return(quote)
 
-def update_encouragements(encouraging_message):
-    if "encouragements" in db.keys():
+def get_gif():
+    apikey = "0ML4KXVQP4HS"
+    lmt = 8
+    search_term = "love"
+    response = requests.get("https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (search_term, apikey, lmt))
+    json_data = json.loads(response.content)
+
+
 
 # Bot
 @client.event
@@ -47,18 +93,28 @@ async def on_message(message):
     
     msg = message.content
     
-    # Hello 
+    # Greetings
     if message.content.startswith('$hello'):
-        await message.channel.send('Hello !')
+        await message.channel.send(random.choice
+                                   (starter_greetings))
       
     # Random insiparation  
     if message.content.startswith('$inspire'):
         quote = get_quote()
         await message.channel.send(quote)
-        
+    
+    if any(word in msg for word in love_words):
+        await message.channel.send(random.choice
+                                   (starter_love))
+    
+    if any(word in msg for word in hello_words):
+        await message.channel.send(random.choice
+                                   (starter_greetings))
+    
+    # Encouragement
     if any(word in msg for word in sad_words):
         await message.channel.send(random.choice
                                    (starter_encouragements))
 
 # Run the client on the server
-client.run('')
+client.run('ODA1MTkxMDY3ODk2Nzc0NjU3.YBXSng.MBQwYTUIZFeT8snholEImM6rSYo')
